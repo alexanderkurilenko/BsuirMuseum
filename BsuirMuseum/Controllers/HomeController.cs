4649f106eab1f41cca1c1d8b8b05373a4121671d
alexanderkurilenko <alex080397@gmail.com>
2017-10-15 04:00:15 +0300

    initial

---
+++ BsuirMuseum/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BsuirMuseum.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult TimeLine()
+        {
+            return View();
+        }
+    }
+}
